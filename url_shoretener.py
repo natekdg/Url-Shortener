c06b23cba8b75cee0aa57d762acde3aa2f4e7552
nathan <nathankdgray@gmail.com>
2023-06-24 16:36:24 -0700

    shortener update
    - code has been added for the url shortener

---
+++ url_shoretener.py
@@ -0,0 +1,11 @@
+import pyshorteners
+
+#function for long url to short url
+def shorten_url(long_url):
+    shortener = pyshorteners.Shortener()
+    shortened_url = shortener.tinyurl.short(long_url)
+    return shortened_url
+
+long_url = input("Enter original URL Here: ")
+short_url = shorten_url(long_url)
+print("Shortened URL:", short_url)
