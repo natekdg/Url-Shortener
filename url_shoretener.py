f0217a87c24d8f15ffb338109441c198a96a736a
nathan <nathankdgray@gmail.com>
2023-06-24 20:14:49 -0700

    Update url_shoretener.py

--- url_shoretener.py
+++ url_shoretener.py
@@ -1,11 +1,13 @@
 import pyshorteners
 
-#function for long url to short url
+
+# function for long url to short url
 def shorten_url(long_url):
     shortener = pyshorteners.Shortener()
     shortened_url = shortener.tinyurl.short(long_url)
     return shortened_url
 
-long_url = input("Enter original URL Here: ")
+
+long_url = input('Enter original URL Here: ')
 short_url = shorten_url(long_url)
-print("New URL: ", short_url)
+print('New URL: ', short_url)
